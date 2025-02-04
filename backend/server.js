7dd6d24b0d6f4fd4352987961b3383e8af45c14e
unknown <rudar.arora12@gmail.com>
2025-02-04 12:46:07 +0530

    Milestone-3

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ process.on(" uncaughtException", (err) => {
 //config
 if (process.env.NODE_ENV != "PRODUCTION") {
   require("dotenv").config({
-    path: "backend/config/.env",
+    path: "config/.env",
   });
 }
 //connect db
